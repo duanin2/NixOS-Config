29b13d8a9978eaacf6ddec17775a62c99112fc7f
duanin2 <duanin2@gmail.com>
2023-08-19 11:15:52 +0200

    Use Catppuccin theme files for Alacritty iinstead of Catppuccin Base16

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -13,49 +13,18 @@
       family = "monospace";
       size = 10;
     };
-    colors = {
-      primary = {
-        background = "#${config.colorScheme.colors.base00}";
-        foreground = "#${config.colorScheme.colors.base05}";
-      };
-      cursor = {
-        text = "#${config.colorScheme.colors.base00}";
-        cursor = "#${config.colorScheme.colors.base05}";
-      };
-      normal = {
-        black = "#${config.colorScheme.colors.base00}";
-        red = "#${config.colorScheme.colors.base08}";
-        green = "#${config.colorScheme.colors.base0B}";
-        yellow = "#${config.colorScheme.colors.base0A}";
-        blue = "#${config.colorScheme.colors.base0D}";
-        magenta = "#${config.colorScheme.colors.base0E}";
-        cyan = "#${config.colorScheme.colors.base0C}";
-        white = "#${config.colorScheme.colors.base05}";
-      };
-      bright = {
-        black = "#${config.colorScheme.colors.base03}";
-        red = "#${config.colorScheme.colors.base08}";
-        green = "#${config.colorScheme.colors.base0B}";
-        yellow = "#${config.colorScheme.colors.base0A}";
-        blue = "#${config.colorScheme.colors.base0D}";
-        magenta = "#${config.colorScheme.colors.base0E}";
-        cyan = "#${config.colorScheme.colors.base0C}";
-        white = "#${config.colorScheme.colors.base07}";
-      };
-      indexed_colors = [
-        { index = 16; color = "#${config.colorScheme.colors.base09}"; }
-        { index = 17; color = "#${config.colorScheme.colors.base0F}"; }
-        { index = 18; color = "#${config.colorScheme.colors.base01}"; }
-        { index = 19; color = "#${config.colorScheme.colors.base02}"; }
-        { index = 20; color = "#${config.colorScheme.colors.base04}"; }
-        { index = 21; color = "#${config.colorScheme.colors.base06}"; }
-      ];
-    };
     cursor = {
       style = {
         shape = "Beam";
         blinking = "Always";
       };
     };
+
+    import = [
+      (pkgs.fetchurl {
+        url = "https://github.com/catppuccin/alacritty/raw/main/catppuccin-frappe.yml";
+        hash = "";
+      })
+    ];
   };
 }
