5cd56429be828ba40470819f028e901da37fa5a0
duanin2 <duanin2@gmail.com>
2023-10-23 11:48:02 +0200

    eww dynamic icons

--- nixos/Duanin2Laptop/home-manager/duanin2/eww/default.nix
+++ nixos/Duanin2Laptop/home-manager/duanin2/eww/default.nix
@@ -23,5 +23,14 @@
     };
   };
 
-  home.packages = with pkgs; [ eww-wayland ];
+  home.packages = with pkgs; [
+    (eww-wayland.overrideAttrs (old: rec {
+      patches = (old.patches or []) ++ [
+        (fetchpatch {
+          url = "https://github.com/ralismark/eww/pull/4.patch";
+          hash = "sha256-tORGA5lXwDE3n3rYhaxBdt1Z4ZxBENTQIJhv3Hbl5yU=";
+        })
+      ];
+    }))
+  ];
 }
