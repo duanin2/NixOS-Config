2b12b7995b580f7c7fe5f2bf93913654f3f4bea4
Dušan Till <tilldusan30@gmail.com>
2024-01-20 21:16:18 +0100

    Fix missing state version

--- SchoolServer/default.nix
+++ SchoolServer/default.nix
@@ -3,7 +3,12 @@
     ./modules/nix
   ];
 
-  home = {};
+  home = {
+    username = "tilldu22";
+    homeDirectory = "/home/${config.home.username}";
+
+    stateVersion = "24.01";
+  };
 
   programs.home-manager.enable = true; # install Home Manager
 }
