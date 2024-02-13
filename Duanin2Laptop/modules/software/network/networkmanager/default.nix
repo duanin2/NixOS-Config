ccab50befeef0d1876e17cbfc05255881a39c421
Dušan Till <tilldusan30@gmail.com>
2024-02-13 20:58:44 +0100

    Legacy sigalg OpenSSL on WPA2 Enterprise networks fix

--- Duanin2Laptop/modules/software/network/networkmanager/default.nix
+++ Duanin2Laptop/modules/software/network/networkmanager/default.nix
@@ -1,7 +1,23 @@
-{ ... }: {
+{ pkgs, ... }: {
 	networking.networkmanager = {
 		enable = true;
 
 		dns = "systemd-resolved";
 	};
+
+	networking.wireless.extraConfig = ''
+openssl_ciphers=DEFAULT@SECLEVEL=0
+	'';
+
+	systemd.services.wpa_supplicant.environment.OPENSSL_CONF = pkgs.writeText "openssl.cnf" ''
+openssl_conf = openssl_init
+[openssl_init]
+ssl_conf = ssl_sect
+[ssl_sect]
+system_default = system_default_sect
+[system_default_sect]
+Options = UnsafeLegacyRenegotiation
+[system_default_sect]
+CipherString = Default:@SECLEVEL=0
+	'';
 }
