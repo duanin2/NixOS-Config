2fbcdc7312bad70a94d63727632a049c64602b2f
Dušan Till <tilldusan30@gmail.com>
2024-02-16 19:23:27 +0100

    mesa-git extra drivers

--- Duanin2Laptop/modules/software/mesa/default.nix
+++ Duanin2Laptop/modules/software/mesa/default.nix
@@ -3,6 +3,6 @@
     enable = true;
 
     extraPackages = with pkgs; [ mesa_git.opencl intel-media-driver vaapiIntel vaapiVdpau libvdpau-va-gl intel-ocl intel-compute-runtime ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [ pkgs.mesa32_git.opencl intel-media-driver vaapiIntel vaapiVdpau libvdpau-va-gl intel-ocl intel-compute-runtime ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [ intel-media-driver vaapiIntel vaapiVdpau libvdpau-va-gl ];
   };
 }
