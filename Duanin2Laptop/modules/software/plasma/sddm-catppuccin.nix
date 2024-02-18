ccd1dfc97f05f6313cc3737e0fac8a1a6989d079
Dušan Till <tilldusan30@gmail.com>
2024-02-18 19:37:55 +0100

    sddm-catppuccin

--- Duanin2Laptop/modules/software/plasma/sddm-catppuccin.nix
+++ Duanin2Laptop/modules/software/plasma/sddm-catppuccin.nix
@@ -14,8 +14,8 @@ stdenvNoCC.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "catppuccin";
     repo = "sddm";
-    rev = "95bfcba80a3b0cb5d9a6fad422a28f11a5064991";
-    hash = "sha256-Jf4xfgJEzLM7WiVsERVkj5k80Fhh1edUl6zsSBbQi6Y=";
+    ref = "main";
+    hash = "";
   };
 
   dontConfigure = true;
