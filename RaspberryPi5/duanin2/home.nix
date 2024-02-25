cf7d71acb185e09caf276fb366d53302145bd912
Dušan Till <tilldusan30@gmail.com>
2024-02-25 13:02:28 +0100

    fix incorrect common path for home-manager configs

--- RaspberryPi5/duanin2/home.nix
+++ RaspberryPi5/duanin2/home.nix
@@ -1,7 +1,7 @@
 { ... }: {
   imports = [
-    ../common/duanin2/modules/shell/nushell.nix
-		../common/duanin2/modules/shell/starship
+    ../../common/duanin2/modules/shell/nushell.nix
+		../../common/duanin2/modules/shell/starship
   ];
 
   home.stateVersion = "24.05";
