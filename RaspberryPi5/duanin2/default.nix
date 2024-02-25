d31b39e44db197581524fd740770f28c180c6623
Dušan Till <tilldusan30@gmail.com>
2024-02-25 22:24:03 +0100

    Add the ssh authorized keys

--- RaspberryPi5/duanin2/default.nix
+++ RaspberryPi5/duanin2/default.nix
@@ -7,6 +7,9 @@
 			"wheel"
 		];
 		shell = pkgs.nushellFull;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDtepAfiQJkST5QEaUFv+y3tuOsXig53eB++alzn198TsXswPEFQmEHT3/KuIXIirLnaPVu9/lvxsyj3kgb4xGskXnzyXMC4snKJIQRg+SujIL2aE3u3dhVqzkVqj8ECLqd2jzAtcC95AzUdFR6liI9lTQ9KM1nmzK/SggrUgJydEKQxJmgBE5SzJ9kVoncpW+xNtMghsDzvuOW/N1zKMR3tB12tew32nenMFrq0FyPcIU+ZEB6jiCwEqoQ64GQEo8APKNGGnpuZ2lAQMVwLLH0Vw/PK2jXgaW70fCivGn3E6E4wM2t009CFggV6A2haOymrhdVSy6aE1VHVQf9HPqJm+GMyETfhy1iqPNPmdmjOAqKIS5u2mTbaUYURI5wtYQbAqr/JUla9Hg90OMMoQreCSe74H54lW0ljPVijajc4txdCImX9MU/bWlwmnE3t9n1NBh15g9P+QRFwPIkCpaZ+qXWrotVj+NOvEavNiJouldS7iQ8LdWthjS/SFV9ey0= duanin2@Duanin2Aspire"
+    ];
 	};
 
 	home-manager.users."duanin2" = import ./home.nix;
