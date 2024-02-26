8c7d67fa3fb7e2260e8645679f52c14ebe24c072
Dušan Till <tilldusan30@gmail.com>
2024-02-26 20:55:44 +0100

    Fix transmission

--- RaspberryPi5/modules/software/web/transmission/default.nix
+++ RaspberryPi5/modules/software/web/transmission/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   services.transmission = {
     enable = true;
     package = pkgs.transmission_4;
 
     webHome = pkgs.flood-for-transmission;
-    openPeerPort = true;
+    openPeerPorts = true;
     home = "/torrent";
     credentialsFile = "/var/lib/secrets/transmission/settings.json";
   };
@@ -12,6 +12,11 @@
   services.nginx.virtualHosts."transmission.duanin2.top" = {
     useACMEHost = "duanin2.top";
 
-    
+    listen = [
+      {
+        addr = "127.0.0.1";
+        port = config.services.transmission.port;
+      }
+    ];
   };
 }
