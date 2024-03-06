221de8bc227647497e4cb83b7626f5b720854505
Dušan Till <tilldusan30@gmail.com>
2024-03-06 19:41:30 +0100

    fix Raspberry Pi vendor kernel

--- RaspberryPi5/modules/software/kernel/vendor-latest.nix
+++ RaspberryPi5/modules/software/kernel/vendor-latest.nix
@@ -1,15 +1,21 @@
 { inputs, pkgs, ... }: {
-  boot.kernelPackages = pkgs.linuxPackagesFor (inputs.rpi5.legacyPackages.${pkgs.system}.linux_rpi5.overrideDerivation (old: let
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage "${inputs.rpi5}/linux-rpi.nix" (let
     modDirVersion = "6.8-rc7";
   in {
     inherit modDirVersion;
     version = modDirVersion;
 
     src = pkgs.fetchFromGitHub {
-      owner = old.src.owner;
-      repo = old.src.repo;
+      owner = "raspberrypi";
+      repo = "linux";
       rev = "90d35da658da8cff0d4ecbb5113f5fac9d00eb72";
       hash = "sha256-wPsC8/cUscTbBrgxYu2Y2h0qdr8a0BbJqvl3avbTMWE=";
     };
+
+    kernelPatches = with pkgs.kernelPatches; [
+      bridge_stp_helper
+      request_key_helper
+    ];
+    rpiVersion = 5;
   }));
 }
