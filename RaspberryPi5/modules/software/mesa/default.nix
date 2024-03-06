4b9e6664115ae687d857b4c028a18694b1562c6f
Dušan Till <tilldusan30@gmail.com>
2024-03-06 18:28:34 +0100

    Mesa changes

--- RaspberryPi5/modules/software/mesa/default.nix
+++ RaspberryPi5/modules/software/mesa/default.nix
@@ -1,6 +1,27 @@
-{ ... }: {
+{ pkgs, ... }: let
+  override = {
+    enable = true;
+    # driSupport32Bit = true;
+
+    package = pkgs.mesa.override {
+      galliumDrivers = [
+        "swrast"
+        "virgl"
+        "zink"
+        "v3d"
+      ];
+      vulkanDrivers = [
+        "swrast"
+        "virtio"
+        "broadcom"
+      ];
+    };
+  };
+in {
   hardware.opengl = {
     enable = true;
     # driSupport32Bit = true;
+
+    package = pkgs.mesa.override override;
   };
 }
