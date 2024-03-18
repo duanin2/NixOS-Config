de7138af8a4e2f42654dc65c31a4d386101c4bc1
Dušan Till <tilldusan30@gmail.com>
2024-03-18 22:33:25 +0100

    Fix RPi5 ssh config

--- Duanin2Aspire/duanin2/modules/ssh/rpi5.nix
+++ Duanin2Aspire/duanin2/modules/ssh/rpi5.nix
@@ -1,9 +1,8 @@
 { pkgs, ... }: {
   programs.ssh.matchBlocks."RPi5" = {
     host = "RPi5";
-    hostname = "rpi.duanin2.top";
+    hostname = "duanin2.top";
     port = 22;
     user = "duanin2";
-    proxyCommand = "${pkgs.cloudflared}/bin/cloudflared access ssh --hostname %h";
   };
 }
