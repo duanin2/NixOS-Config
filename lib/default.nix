562e1720c80d9bd6af30f9afc6135c4b0154325b
Dušan Till <tilldusan30@gmail.com>
2024-03-23 16:40:00 +0100

    Add convertor fuunction

--- lib/default.nix
+++ lib/default.nix
@@ -16,4 +16,26 @@ final: prev: {
   contains = query: list: builtins.any (element: query == element) list;
 
   isNumber = value: builtins.isInt value || builtins.isFloat value;
+
+  convertor = {
+		default,
+		isAttrs ? default,
+		isBool ? default,
+		isFloat ? default,
+		isFunction ? default,
+		isInt ? default,
+		isList ? default,
+		isNull ? default,
+		isPath ? default,
+		isString ? default
+	}: value: if builtins.isAttrs value then isAttrs
+	else if builtins.isBool value then isBool
+	else if builtins.isFloat value then isFloat
+	else if builtins.isFunction value then isFunction
+	else if builtins.isInt value then isInt
+	else if builtins.isList value then isList
+	else if builtins.isNull value then isNull
+	else if builtins.isPath value then isPath
+	else if builtins.isString value then isString
+	else default;
 }
