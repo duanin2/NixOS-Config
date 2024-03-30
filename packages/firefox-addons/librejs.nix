f6561f2fb7fc512fec75b43677ce5036225f647a
Dušan Till <tilldusan30@gmail.com>
2024-03-30 20:13:05 +0100

    librejs add hash

--- packages/firefox-addons/librejs.nix
+++ packages/firefox-addons/librejs.nix
@@ -5,14 +5,14 @@
   addonId = "jid1-KtlZuoiikVfFew@jetpack";
 
   url = "https://ftp.gnu.org/gnu/librejs/librejs-${version}.xpi";
-  sha256 = "";
+  sha256 = "sha256-YMWxZjqyUH3BIRLkQHa7mcj+7Qd3tNQO50ua7xsMclY=";
 
   meta = with lib; {
     description = "LibreJS is a free add-on for GNU IceCat and other Mozilla-based browsers. It blocks nonfree nontrivial JavaScript while allowing JavaScript that is free and/or trivial.";
     longDescription = "GNU LibreJS aims to address the JavaScript problem described in Richard Stallman's article The JavaScript Trap. LibreJS is a free add-on for GNU IceCat and other Mozilla-based browsers. It blocks nonfree nontrivial JavaScript while allowing JavaScript that is free and/or trivial.";
 
     homepage = "https://www.gnu.org/software/librejs/";
-    license = licences.gpl3Plus;
+    license = licenses.gpl3Plus;
     maintainers = with maintainers; [ duanin2 ];
     platforms = platforms.all;
   };
