00822b052fa5c8163155f7caa5a1476eef0b4dce
Dušan Till <tilldusan30@gmail.com>
2024-03-30 20:13:16 +0100

    Go back to rpi5 vendor kernel.

--- RaspberryPi5/default.nix
+++ RaspberryPi5/default.nix
@@ -22,7 +22,7 @@
     ./modules/software/web/invidious
 
     # software/kernel
-    ./modules/software/kernel/vendor-nixpkgs.nix
+    ./modules/software/kernel/vendor.nix
 
     # software/shell
     ../common/modules/software/shell/nushell.nix
