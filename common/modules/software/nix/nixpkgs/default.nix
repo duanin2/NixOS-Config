5b3bbd6b5a834702191d16b7cdde83a000e4d9df
Duanin2 <tilldusan30@gmail.com>
2024-04-01 08:56:58 +0000

    Invidious

--- common/modules/software/nix/nixpkgs/default.nix
+++ common/modules/software/nix/nixpkgs/default.nix
@@ -7,6 +7,17 @@
 			(final: prev: {
 				xz = stagingPkgs.xz.override { inherit (final) lib stdenv fetchurl writeScript testers; };
 			})
+			(final: prev: {
+				invidious = prev.invidious.overrideAttrs (_oldAttrs: {
+					src = prev.fetchFromGitHub {
+						owner = "iv-org";
+						repo = "invidious";
+						fetchSubmodules = true;
+						rev = "08390acd0c17875fddb84cabba54197a5b5740e4";
+						sha256 = "sha256-75C/ImX/PYikVdSO4rZM/aYyEgx6pU90BHNeRFfcsDM=";
+					};
+				});
+			})
 		];
 	};
 }
