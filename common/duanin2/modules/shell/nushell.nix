364b245b6d5f0407b5f185aabf50c6edcea1d71b
Dušan Till <tilldusan30@gmail.com>
2024-04-13 18:02:11 +0200

    nushell broken config

--- common/duanin2/modules/shell/nushell.nix
+++ common/duanin2/modules/shell/nushell.nix
@@ -1,9 +1,23 @@
 { config, lib, persistDirectory, ... }: {
-  programs = (lib.setAttrs { enableNushellIntegration = true; } config.programs) // {
+  /*
+  programs = lib.setAttrs { enableNushellIntegration = true; } config.programs // {
     nushell = {
       enable = true;
     };
   };
+  */
+
+  programs.nushell = {
+    enable = true;
+  };
+
+  programs.yazi.enableNushellIntegration = true;
+  programs.atuin.enableNushellIntegration = true;
+  programs.direnv.enableNushellIntegration = true;
+  programs.zoxide.enableNushellIntegration = true;
+  programs.carapace.enableNushellIntegration = true;
+  programs.keychain.enableNushellIntegration = true;
+  programs.starship.enableNushellIntegration = true;
 
   home.persistence.${persistDirectory} = {
     files = [ ".config/nushell/history.txt" ];
