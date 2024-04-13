ea45680796023cd63ba7800177f2058b8b3bb650
Dušan Till <tilldusan30@gmail.com>
2024-04-13 17:57:29 +0200

    unsuccessful attemt to fix brokun home directory

--- Duanin2Aspire/modules/software/impermanence/default.nix
+++ Duanin2Aspire/modules/software/impermanence/default.nix
@@ -13,5 +13,22 @@
       "/etc/machine-id"
       { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
+    users.duanin2 = {
+      directories = [
+        ".local/state/nix/profiles"
+      ];
+    };
+  };
+
+  system.activationScripts."fix-home-perms" = {
+    deps = [ "users" ];
+    text = ''
+      if ! [[ -e /home/duanin2 ]]; then
+        mkdir -p /home/duanin2
+      fi
+      if [[ $(stat -c "%U" /home/duanin2/) != duanin2 ]]; then
+        chown -R duanin2:users /home/duanin2
+      fi
+    '';
   };
 }
