ac53c90e6ba3f18b6e13b38d6fd346855a69a416
Dušan Till <tilldusan30@gmail.com>
2024-04-13 18:02:29 +0200

    update Prism Launcher

--- Duanin2Aspire/duanin2/modules/games/prismlauncher/default.nix
+++ Duanin2Aspire/duanin2/modules/games/prismlauncher/default.nix
@@ -1,24 +1,24 @@
 { inputs, pkgs, persistDirectory, ... }: {
-  imports = [
-    ../.
-  ];
+	imports = [
+		../.
+	];
 
-  home.packages = with pkgs; [
-    (prismlauncher.override {
-      prismlauncher-unwrapped = (prismlauncher-unwrapped.overrideAttrs (old: {
-        version = "8.0";
-        patches = (old.patches or []) ++ [
-          (pkgs.fetchpatch {
-            url = "https://github.com/duanin2/PKGBUILD/raw/main/prismlauncher-cracked/allowOffline.patch";
-            hash = "sha256-3iO0AUHmS297CJOXTDpKFPBVhz8TpnbfmIBJ3Aa7MZQ=";
-          })
-        ];
-      }));
-    })
-    kdialog
-  ];
+	home.packages = with pkgs; [
+		(prismlauncher.override {
+			prismlauncher-unwrapped = (prismlauncher-unwrapped.overrideAttrs (old: {
+				version = "8.2";
+				patches = (old.patches or []) ++ [
+					(pkgs.fetchpatch {
+						url = "https://github.com/duanin2/PKGBUILD/raw/main/prismlauncher-cracked/allowOffline.patch";
+						hash = "sha256-3iO0AUHmS297CJOXTDpKFPBVhz8TpnbfmIBJ3Aa7MZQ=";
+					})
+				];
+			}));
+		})
+		kdialog
+	];
 
-  home.persistence.${persistDirectory} = {
-    directories = [ ".local/share/PrismLauncher" ];
-  };
+	home.persistence.${persistDirectory} = {
+		directories = [ ".local/share/PrismLauncher" ];
+	};
 }
