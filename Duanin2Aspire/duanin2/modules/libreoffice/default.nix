92977950a034efc85d0d9fbad526989e88164b9d
Dušan Till <tilldusan30@gmail.com>
2024-04-13 18:03:19 +0200

    persistence

--- Duanin2Aspire/duanin2/modules/libreoffice/default.nix
+++ Duanin2Aspire/duanin2/modules/libreoffice/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, persistDirectory, ... }: {
   home.packages = with pkgs; [
     libreoffice-qt
   ];
@@ -6,4 +6,8 @@
   home.sessionVariables = {
     "SAL_USE_VCLPLUGIN" = "qt5";
   };
+
+  home.persistence.${persistDirectory}.directories = [
+    ".config/libreoffice/4/user"
+  ];
 }
