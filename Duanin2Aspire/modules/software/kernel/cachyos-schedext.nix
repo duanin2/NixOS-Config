21380916afa8c187a281b66416ec078a1dd39d42
Dušan Till <tilldusan30@gmail.com>
2024-04-15 15:31:11 +0200

    Remove deprecated kernel

--- Duanin2Aspire/modules/software/kernel/cachyos-schedext.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  # Deprecated, as the sched-ext patches are now in 'cachyos' variant
-
-  boot.kernelPackages = pkgs.linuxPackages_cachyos-sched-ext;
-  environment.systemPackages = with pkgs; [
-    scx
-  ];
-}
