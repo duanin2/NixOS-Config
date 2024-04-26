66de20ce9dc550d2c427afbee621c0144e9d1aef
Dušan Till <tilldusan30@gmail.com>
2024-04-26 21:11:02 +0200

    Hashes

--- common/duanin2/modules/Mozilla/thunderbird.nix
+++ common/duanin2/modules/Mozilla/thunderbird.nix
@@ -5,7 +5,7 @@
 			package = pkgs.thunderbird;
 			src = pkgs.fetchurl {
 				url = "https://raw.githubusercontent.com/HorlogeSkynet/thunderbird-user.js/master/user.js";
-				hash = "sha256-66B1yLQkQnydAUXD7KGt32OhWSYcdWX+BUozrgW9uAg=";
+				hash = "sha256-2M7EOosZI9cYgCkOMn+aoHDsRPOrZQn/TyrtbkjSqNE=";
 			};
 		};
 
