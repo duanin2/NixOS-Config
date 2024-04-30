e44a3ab3e9ab7c06c308e7a570795bc90d0d8dea
Dušan Till <tilldusan30@gmail.com>
2024-04-30 09:01:46 +0200

    Fix impermanence onRPi5

--- RaspberryPi5/duanin2/home.nix
+++ RaspberryPi5/duanin2/home.nix
@@ -1,4 +1,7 @@
-{ ... }: {
+{ config, ... }: let
+	homeDirectory = config.home.homeDirectory or ("/home/${config.home.username or "duanin2"}");
+	persistDirectory = "/persist" + homeDirectory;
+in {
   imports = [
     ../../common/duanin2/modules/shell/nushell.nix
 		../../common/duanin2/modules/shell/starship
@@ -6,4 +9,6 @@
   ];
 
   home.stateVersion = "23.11";
+
+  _module.args = { inherit homeDirectory persistDirectory; };
 }
