635513c52371ce7b88bc8beb3f78fe9631bc4848
Dušan Till <tilldusan30@gmail.com>
2024-04-30 08:56:30 +0200

    new kernrel and has

--- RaspberryPi5/modules/software/kernel/vendor-latest.nix
+++ RaspberryPi5/modules/software/kernel/vendor-latest.nix
@@ -1,25 +1,27 @@
 { inputs, lib, pkgs, ... }: {
-  imports = [
-    ../../../../common/iso/no-zfs.nix
-  ];
-  
-  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.callPackage "${inputs.rpi5}/linux-rpi.nix" (let
-    modDirVersion = "6.8.5";
-  in {
-    inherit modDirVersion;
-    version = modDirVersion;
+	imports = [
+	  ../../../../common/iso/no-zfs.nix
+	];
+	
+	boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.callPackage "${inputs.rpi5}/linux-rpi.nix" (let
+		modDirVersion = "6.8.7";
+	in {
+		argsOverride = {
+			inherit modDirVersion;
+			version = "${modDirVersion}-git";
 
-    src = pkgs.fetchFromGitHub {
-      owner = "raspberrypi";
-      repo = "linux";
-      rev = "b95f2066a910ace64787dc4f3e1dfcb2e7e71718"; # Linux 6.8.5
-      hash = "";
-    };
+			src = pkgs.fetchFromGitHub {
+				owner = "raspberrypi";
+				repo = "linux";
+				rev = "12dadc409c2bd8538c6ee0e56e191efde6d92007"; # Linux 6.8.7
+				hash = "sha256-eqterW9A+opVmUvDGCNLBV3I2Vpf1DAE4+RHn6VxGMg=";
+			};
+		};
 
-    kernelPatches = with pkgs.kernelPatches; [
-      bridge_stp_helper
-      request_key_helper
-    ];
-    rpiVersion = 5;
-  })));
+		kernelPatches = with pkgs.kernelPatches; [
+			bridge_stp_helper
+			request_key_helper
+		];
+		rpiVersion = 5;
+	})));
 }
