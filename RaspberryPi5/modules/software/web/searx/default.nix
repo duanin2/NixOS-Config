163f56e98c3989166f907a3edb0298f849c172e5
Dušan Till <tilldusan30@gmail.com>
2024-05-25 20:18:29 +0200

    unix://

--- RaspberryPi5/modules/software/web/searx/default.nix
+++ RaspberryPi5/modules/software/web/searx/default.nix
@@ -68,7 +68,7 @@
     
     locations."/" = {
       extraConfig = ''
-uwsgi_pass unix://${config.services.searx.uwsgiConfig.socket};
+uwsgi_pass ${config.services.searx.uwsgiConfig.socket};
 
 uwsgi_param QUERY_STRING $query_string;
 uwsgi_param REQUEST_METHOD $request_method;
