aa26cbadce29a99ef2513bb8064989365038cd6f
Dušan Till <tilldusan30@gmail.com>
2024-05-28 09:02:21 +0200

    find the key and X forwarding

--- Duanin2Aspire/duanin2/modules/ssh/rpi5.nix
+++ Duanin2Aspire/duanin2/modules/ssh/rpi5.nix
@@ -1,8 +1,12 @@
-{ ... }: {
+{ lib, pkgs, homeDirectory, ... }: {
   programs.ssh.matchBlocks."RPi5" = {
     host = "RPi5";
     hostname = "duanin2.top";
     port = 22;
     user = "duanin2";
+    identityFile = "${homeDirectory}/.ssh/rpi5";
+    
+    forwardX11Trusted = true;
+    forwardAgent = true;
   };
 }
