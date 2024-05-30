93c1369886495f569161c38b0a577b4c8105694e
Dušan Till <tilldusan30@gmail.com>
2024-05-30 19:02:48 +0200

    Keep Pidgin config

--- common/duanin2/modules/pidgin/default.nix
+++ common/duanin2/modules/pidgin/default.nix
@@ -1,13 +1,15 @@
-{ pkgs, ... }: {
+{ persistDirectory, pkgs, ... }: {
   programs.pidgin = {
     enable = true;
 
-    plugins = with pkgs.pidginPlugins; [
-      tdlib-purple
+    plugins = with pkgs.pidginPackages; [
+      # tdlib-purple # Unmaintained, apparently
       pidgin-latex
       purple-discord
       purple-facebook
       pidgin-opensteamworks
     ];
   };
+
+  home.persistence.${persistDirectory}.directories = [ ".purple" ];
 }
