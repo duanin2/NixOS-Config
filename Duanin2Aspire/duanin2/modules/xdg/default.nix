0a2a63922e2ae219705bd5b3d7332c73a4673353
Dušan Till <tilldusan30@gmail.com>
2024-06-05 20:51:50 +0200

    XDG user directories

--- Duanin2Aspire/duanin2/modules/xdg/default.nix
+++ Duanin2Aspire/duanin2/modules/xdg/default.nix
@@ -1,5 +1,20 @@
-{ ... }: {
+{ homeDirectory, ... }: {
   xdg = {
     enable = true;
+
+    userDirs = {
+      enable = true;
+      
+      createDirectories = true;
+
+      desktop = "${homeDirectory}/Plocha";
+      documents = "${homeDirectory}/Dokumenty";
+      download = "${homeDirectory}/Stažené";
+      music = "${homeDirectory}/Hudba";
+      pictures = "${homeDirectory}/Obrázky";
+      publicShare = "${homeDirectory}/Veřejné";
+      templates = "${homeDirectory}/Šablony";
+      videos = "${homDirectory}/Videa";
+    };
   };
 }
