463a03bce548869f9366f776ceae3bfd71ad3196
Dušan Till <tilldusan30@gmail.com>
2024-06-05 20:54:16 +0200

    Fix me forgetting environment

--- RaspberryPi5/modules/software/ssh/default.nix
+++ RaspberryPi5/modules/software/ssh/default.nix
@@ -10,7 +10,7 @@
     openFirewall = true;
   };
 
-  persistence."/persist".files = [
+  environment.persistence."/persist".files = [
     "/etc/ssh/ssh_host_ed25519_key"
     "/etc/ssh/ssh_host_ed25519_key.pub"
     "/etc/ssh/ssh_host_rsa_key"
