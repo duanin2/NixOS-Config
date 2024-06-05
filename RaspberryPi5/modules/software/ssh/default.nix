865a4a62849dbbf6c79cba0af9af8f209673655e
Dušan Till <tilldusan30@gmail.com>
2024-06-05 20:52:24 +0200

    Fix the Raspberry Pi 5 forgetting its host keys on reboot

--- RaspberryPi5/modules/software/ssh/default.nix
+++ RaspberryPi5/modules/software/ssh/default.nix
@@ -9,4 +9,11 @@
     };
     openFirewall = true;
   };
+
+  persistence."/persist".files = [
+    "/etc/ssh/ssh_host_ed25519_key"
+    "/etc/ssh/ssh_host_ed25519_key.pub"
+    "/etc/ssh/ssh_host_rsa_key"
+    "/etc/ssh/ssh_host_rsa_key.pub"
+  ];
 }
