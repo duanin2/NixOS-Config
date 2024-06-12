6505cc3b74b34489d1b92802f28c221c6bd4f9a6
Dušan Till <tilldusan30@gmail.com>
2024-06-12 18:51:08 +0200

    GNUnet

--- common/modules/software/gnunet/default.nix
+++ common/modules/software/gnunet/default.nix
@@ -3,8 +3,16 @@
 in {
   services.gnunet = {
     enable = true;
+
+    extraConfig = ''
+[arm]
+START_SYSTEM_SERVICES = YES
+START_USER_SERVICES = NO
+    '';
   };
 
   networking.firewall.allowedUDPPorts = [ cfg.udp.port ];
   networking.firewall.allowedTCPPorts = [ cfg.tcp.port ];
+
+  users.users."duanin2".extraGroups = [ "gnunet" ];
 }
