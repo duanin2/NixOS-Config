7832481416d59b9f5c120cf1e57a8edebbd2d843
Dušan Till <tilldusan30@gmail.com>
2024-06-29 22:46:13 +0200

    Prism Launcher move allowOffline patch

--- Duanin2Aspire/duanin2/modules/games/prismlauncher/default.nix
+++ Duanin2Aspire/duanin2/modules/games/prismlauncher/default.nix
@@ -6,12 +6,9 @@
 	home.packages = with pkgs; [
 		(prismlauncher.override {
 			prismlauncher-unwrapped = (prismlauncher-unwrapped.overrideAttrs (old: {
-				version = "8.2";
+				version = "8.3";
 				patches = (old.patches or []) ++ [
-					(pkgs.fetchpatch {
-						url = "https://github.com/duanin2/PKGBUILD/raw/main/prismlauncher-cracked/allowOffline.patch";
-						hash = "sha256-3iO0AUHmS297CJOXTDpKFPBVhz8TpnbfmIBJ3Aa7MZQ=";
-					})
+					./allowOffline.patch
 				];
 			}));
 		})
