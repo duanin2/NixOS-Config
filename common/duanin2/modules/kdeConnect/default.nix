243dd43b1e04b2614d4e2d49856e9e5f9147120a
Dušan Till <tilldusan30@gmail.com>
2024-06-30 13:55:43 +0200

    Forgot KDE Connect persistence

--- common/duanin2/modules/kdeConnect/default.nix
+++ common/duanin2/modules/kdeConnect/default.nix
@@ -1,7 +1,11 @@
-{ ... }: {
+{ persistDirectory, ... }: {
   services.kdeconnect = {
     enable = true;
 
     indicator = true;
+  };
+
+  home.persistence.${persistDirectory} = {
+    directories = [ ".config/kdeconnect" ];
   };
 }
