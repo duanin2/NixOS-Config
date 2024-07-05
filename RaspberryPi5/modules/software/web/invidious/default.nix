e3eba1ef678f2e29025685689384c1efbb0b4c48
Dušan Till <tilldusan30@gmail.com>
2024-07-05 14:52:15 +0200

    Readd nginx to invidious

--- RaspberryPi5/modules/software/web/invidious/default.nix
+++ RaspberryPi5/modules/software/web/invidious/default.nix
@@ -6,20 +6,16 @@
       enable = true;
     };
 
-    /*
     domain = "invidious.duanin2.top";
     nginx.enable = true;
-    */
 
     settings.db.user = "invidious";
   };
 
-  /*
   services.nginx.virtualHosts.${config.services.invidious.domain} = {
     enableACME = lib.mkForce false;
     useACMEHost = "duanin2.top";
   };
-  */
 
   environment.persistence."/persist" = {
     directories = [
