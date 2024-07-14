1c47c7b6ec08d6a7f65f28fa5751007a8ee47bee
Dušan Till <tilldusan30@gmail.com>
2024-07-14 18:40:07 +0200

    Fix invidious version attribute

--- RaspberryPi5/modules/software/web/invidious/default.nix
+++ RaspberryPi5/modules/software/web/invidious/default.nix
@@ -5,9 +5,9 @@
       versions = {
         invidious = {
           hash = "";
-          version = "v2.2024.05.27-unstable";
+          version = "v2.2024.07.10-unstable";
           rev = "bad92093bff66bfb0281e5276fa0e136a61ba330";
-          date = "2024.05.27";
+          date = "2024.07.10";
           commit = "bad9209";
         };
         videojs = {
@@ -24,6 +24,31 @@
     nginx.enable = true;
 
     settings.db.user = "invidious";
+
+    settings = {
+      popular_enabled = false;
+      statistics_enabled = true;
+      admins = [ "duanin2" ];
+      use_pubsub_feeds = true;
+      cache_annotations = true;
+      playlist_length_limit = 1000;
+
+      default_user_preferences = {
+        locale = "cs";
+        region = "CZ";
+        captions = [ "Czech" "English" "English (auto-generated)" ];
+        feed_menu = [ "Trending" "Subscriptions" "Playlists" ];
+        defaut_home = "Subsciptions";
+        max_results = 50;
+        annotations = true;
+        annotations_subscribed = true;
+        comments = [ "youtube" "reddit" ];
+        player_style = "youtube";
+        quality = "dash";
+        save_player_pos = true;
+        automatic_instance_redirect = true;
+      };
+    };
   };
 
   services.nginx.virtualHosts.${config.services.invidious.domain} = {
