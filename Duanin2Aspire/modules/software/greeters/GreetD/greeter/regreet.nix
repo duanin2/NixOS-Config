7b4264b7b7ce89edb850fdf9959d5bd4535744bc
Dušan Till <tilldusan30@gmail.com>
2024-07-16 19:53:50 +0200

    Use the patched colloid theme on the greeter too

--- Duanin2Aspire/modules/software/greeters/GreetD/greeter/regreet.nix
+++ Duanin2Aspire/modules/software/greeters/GreetD/greeter/regreet.nix
@@ -5,10 +5,14 @@
 
   environment.systemPackages = with pkgs; [
     (pkgs.colloid-gtk-theme.override {
-        tweaks = [ "catppuccin" "rimless" "float" ];
-        colorVariants = [ "dark" ];
-        themeVariants = [ "green" ];
-      })
+      tweaks = [ "catppuccin" "rimless" "float" ];
+      colorVariants = [ "dark" ];
+      themeVariants = [ "green" ];
+    }).overrideAttrs (old: {
+      patches = (old.patches or []) ++ [
+        ./colloid.patch
+      ];
+    })
     (catppuccin-papirus-folders.override {
       flavor = "frappe";
       accent = "green";
@@ -24,7 +28,7 @@
         application_prefer_dark_theme = true;
         cursor_theme_name = "Catppuccin-Frappe-Green-Cursors";
         cursor_theme_size = 24;
-        font_name = "FiraCode Nerd Font Propo 10";
+        font_name = "FiraCode Nerd Font 10";
         icon_theme_name = "Papirus-Dark";
         theme_name = "Colloid-Green-Dark-Catppuccin";
       };
