cb3d251f2151efb94e36ac7f8a162f33ec30239a
Dušan Till <tilldusan30@gmail.com>
2024-07-16 19:28:07 +0200

    Revert "Switch GreetD to Cage"

--- Duanin2Aspire/modules/software/greeters/GreetD/default.nix
+++ Duanin2Aspire/modules/software/greeters/GreetD/default.nix
@@ -1,4 +1,41 @@
-{ inputs, config, lib, pkgs, customPkgs, ... }: {
+{ inputs, config, lib, pkgs, customPkgs, ... }: let
+  hyprlandConfig = pkgs.writeText "greetd-hyprland.conf" ''
+  general {
+    border_size = 0
+    gaps_in = 0
+    gaps_out = 0
+  }
+
+  animations {
+    enabled = false
+    first_launch_animation = false
+  }
+
+  input {
+    kb_model = acer_laptop
+    kb_layout = cz
+    numlock_by_default = true
+  }
+
+  misc {
+    disable_hyprland_logo = true
+    disable_splash_rendering = true
+    force_default_wallpaper = 0
+
+    mouse_move_enables_dpms = true
+    key_press_enables_dpms = true
+
+    disable_autoreload = true
+  }
+
+  env = HYPRCURSOR_THEME, Catppuccin-Frappe-Green-Hyprcursors
+  env = HYPRCURSOR_SIZE, 24
+
+  exec-once = ${pkgs.dbus}/bin/dbus-run-session ${lib.getExe config.programs.regreet.package}
+
+  monitor = eDP-1, 1920x1080@60, 0x0, 1
+  '';
+in {
   imports = [
     ./greeter/regreet.nix
   ];
@@ -13,7 +50,7 @@
     vt = 7;
     settings = {
       default_session = {
-        command = "env XKB_DEFAULT_MODEL=acer_laptop XKB_DEFAULT_LAYOUT=cz XCURSOR_PATH=${pkgs.catppuccin-cursors.frappeGreen} ${lib.getExe pkgs.cage} -d -m extend -- ${pkgs.dbus}/bin/dbus-run-session ${lib.getExe config.programs.regreet.package}";
+        command = "${config.programs.hyprland.package}/bin/Hyprland --config ${hyprlandConfig}";
       };
     };
   };
