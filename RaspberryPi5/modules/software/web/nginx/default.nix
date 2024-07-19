d77ce9811c3c382a03d2029b3b14867adb561e78
Dušan Till <tilldusan30@gmail.com>
2024-07-19 15:59:43 +0200

    Matrix fix 12 aka recommended Nginx settings

--- RaspberryPi5/modules/software/web/nginx/default.nix
+++ RaspberryPi5/modules/software/web/nginx/default.nix
@@ -5,6 +5,10 @@
 
   services.nginx = {
     enable = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+    recommendedProxySettings = true;
 
     virtualHosts = {
       "acmechallenge.duanin2.top" = {
