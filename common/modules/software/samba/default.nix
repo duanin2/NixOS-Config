d5657e714f3de4feadfe6b6bf02f30683ab0039b
Dušan Till <tilldusan30@gmail.com>
2024-07-25 18:14:19 +0200

    Move SMB config

--- common/modules/software/samba/default.nix
+++ common/modules/software/samba/default.nix
@@ -4,8 +4,16 @@
 
     nsswins = true;
     enableWinbindd = true;
+    enableNmbd = true;
+    openFirewall = true;
     extraConfig = ''
     client min protocol = NT1
     '';
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+
+    openFirewall = true;
   };
 }
