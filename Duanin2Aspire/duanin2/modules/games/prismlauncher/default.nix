2b96291af7659094a69159199fcf3e0c8434f549
Dušan Till <tilldusan30@gmail.com>
2024-07-25 18:12:43 +0200

    Fix broken Prism Launcher config patcher

--- Duanin2Aspire/duanin2/modules/games/prismlauncher/default.nix
+++ Duanin2Aspire/duanin2/modules/games/prismlauncher/default.nix
@@ -19,11 +19,7 @@ in {
   home.activation."PrismLauncherCFG" = let
     cfgPath = "${config.xdg.dataHome or "${homeDirectory}/.local/share"}/PrismLauncher/prismlauncher.cfg";
   in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-CFG=$(cat ${cfgPath})
-
-CFG=$(echo $CFG | sed -E -e "s/^UseNativeGLFW=(true|false)$/UseNativeGLFW=${builtins.toString nativeGLFW}/" -e $"s|^CustomGLFWPath=.*$|CustomGLFWPath=${if nativeGLFW then "${pkgs.glfw-wayland-minecraft}/lib/libglfw.so" else ""}|")
-
-echo "$CFG" | run tee ${cfgPath}
+run ${lib.getExe pkgs.gnused} -i.bak -E -e "s/^UseNativeGLFW=(true|false)$/UseNativeGLFW=${builtins.toString nativeGLFW}/" -e "s|^CustomGLFWPath=.*$|CustomGLFWPath=${pkgs.glfw-wayland-minecraft}/lib/libglfw.so|" ${cfgPath}
   '';
 
 	home.persistence.${persistDirectory} = {
