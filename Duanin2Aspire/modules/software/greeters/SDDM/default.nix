692bd01899be673e5ff03cdc71bbba06237c5d03
Dušan Till <tilldusan30@gmail.com>
2024-08-19 18:46:15 +0200

    Forgot to save

---
+++ Duanin2Aspire/modules/software/greeters/SDDM/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }: let
+	sddm-numlock = pkgs.writeText "sddm-numlock-fix" ''
+	[Keyboard]
+	NumLock=0
+	'';
+in {
+  environment.systemPackages = with pkgs; [ (pkgs.callPackage ./sddm-catppuccin.nix { }) ];
+
+  services.xserver.displayManager.sddm = {
+		enable = true;
+
+		theme = "catppuccin-sddm-frappe";
+		autoNumlock = true;
+		wayland = {
+			enable = true;
+
+			compositorCommand = "kwin_wayland --drm --no-lockscreen --no-global-shortcuts --locale1";
+		};
+		settings = {
+			General = {
+				GreeterEnvironment = "QT_WAYLAND_SHELL_INTEGRATION=layer-shell";
+			};
+		};
+	};
+
+	system.activationScripts."sddm-numlock-fix" = {
+		deps = [ "var" ];
+		text = ''
+    mkdir -p /var/lib/sddm/.config
+		ln -s ${sddm-numlock} /var/lib/sddm/.config/kcminputrc
+  	'';
+	};
+}
