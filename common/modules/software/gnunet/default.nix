662a32057388ca471e99fbe74badda0e801f6c25
Dušan Till <tilldusan30@gmail.com>
2024-08-19 18:29:03 +0200

    Disable port forwarding on GNUnet

--- common/modules/software/gnunet/default.nix
+++ common/modules/software/gnunet/default.nix
@@ -4,6 +4,8 @@ in {
   services.gnunet = {
     enable = true;
 
+    udp.port = 0;
+    tcp.port = 0;
     extraOptions = ''
 [arm]
 START_SYSTEM_SERVICES = YES
@@ -11,8 +13,8 @@ START_USER_SERVICES = NO
     '';
   };
 
-  networking.firewall.allowedUDPPorts = [ cfg.udp.port ];
-  networking.firewall.allowedTCPPorts = [ cfg.tcp.port ];
+  networking.firewall.allowedUDPPorts = if cfg.udp.port > 0 then [ cfg.udp.port ] else [ ];
+  networking.firewall.allowedTCPPorts = if cfg.tcp.port > 0 then [ cfg.tcp.port ] else [ ];
 
   system.nssModules = with pkgs; [ gnunet ];
   system.nssDatabases.hosts = lib.mkBefore [ "gns [NOTFOUND=return]" ];
