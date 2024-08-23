52b901e221b5749b462ce2094c9cb9390d67d090
Dušan Till <tilldusan30@gmail.com>
2024-08-23 10:07:01 +0200

    Osu! persistence

--- Duanin2Aspire/duanin2/modules/games/osu!/default.nix
+++ Duanin2Aspire/duanin2/modules/games/osu!/default.nix
@@ -1,3 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, persistDirectory, ... }: {
   home.packages = with pkgs; [ osu-lazer-bin ];
+
+  home.persistence.${persistDirectory}.directories = [ ".local/share/osu" ];
 }
