47c6c290beab2461de21ae942416b849842c53a6
Dušan Till <tilldusan30@gmail.com>
2024-08-24 19:16:57 +0200

    Better konsole config

--- Duanin2Aspire/duanin2/modules/plasma/konsole.nix
+++ Duanin2Aspire/duanin2/modules/plasma/konsole.nix
@@ -3,18 +3,19 @@
     owner = "catppuccin";
     repo = "konsole";
     rev = "3b64040e3f4ae5afb2347e7be8a38bc3cd8c73a8";
-    hash = "";
+    hash = "sha256-d5+ygDrNl2qBxZ5Cn4U7d836+ZHz77m6/yxTIANd9BU=";
   };
 in {
   programs.konsole = {
     enable = true;
 
     customColorSchemes = {
-      catppuccinLatte = catppuccin + /theme/catppuccin-latte.colorscheme;
-      catppuccinFrappe = catppuccin + /theme/catppuccin-frappe.colorscheme;
-      catppuccinMacchiato = catppuccin + /theme/catppuccin-macchiato.colorscheme;
-      catppuccinMocha = catppuccin + /theme/catppuccin-mocha.colorscheme;
+      catppuccinLatte = catppuccin + /themes/catppuccin-latte.colorscheme;
+      catppuccinFrappe = catppuccin + /themes/catppuccin-frappe.colorscheme;
+      catppuccinMacchiato = catppuccin + /themes/catppuccin-macchiato.colorscheme;
+      catppuccinMocha = catppuccin + /themes/catppuccin-mocha.colorscheme;
     };
+    defaultProfile = "Default";
     profiles = {
       Default = {
         colorScheme = "catppuccinFrappe";
