b051c6bc0e0337748d0e9fd01343dd130bab0992
Dušan Till <tilldusan30@gmail.com>
2024-09-07 17:31:03 +0200

    fix Ani-skip

--- Duanin2Aspire/duanin2/modules/mpv/default.nix
+++ Duanin2Aspire/duanin2/modules/mpv/default.nix
@@ -1,5 +1,5 @@
 { inputs, pkgs, ... }: let
-  ani-skip = pkgs.callPackage (inputs.ani-skip-nixpkgs + /pkgs/by-name/an/ani-skip/package.nix);
+  ani-skip = (pkgs.callPackage (inputs.ani-skip-nixpkgs + /pkgs/by-name/an/ani-skip/package.nix) { }).overrideAttrs { passthru.scriptName = "skip.lua"; };
 in {
   home.packages = [ ani-skip ];
   
