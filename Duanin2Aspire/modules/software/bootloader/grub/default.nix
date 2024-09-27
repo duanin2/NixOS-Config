2402021113658055c7b1ebcb049cc77942088941
Dušan Till <tilldusan30@gmail.com>
2024-09-27 08:06:32 +0000

    Add GRUB

---
+++ Duanin2Aspire/modules/software/bootloader/grub/default.nix
@@ -0,0 +1,14 @@
+{ modules, ... }: {
+  imports = [
+    (modules.local.hardware + /efi)
+  ];
+
+  boot.loader.grub = {
+    enable = true;
+    
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    device = "nodev";
+    configurationLimit = 5;
+  };
+}
