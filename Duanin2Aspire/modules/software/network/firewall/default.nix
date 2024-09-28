a04befb436f23f303372e16914f1a2e1fd9005fc
Dušan Till <tilldusan30@gmail.com>
2024-09-28 14:00:09 +0200

    Move all Tor-specific network changes to Tor

--- Duanin2Aspire/modules/software/network/firewall/default.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-  networking.firewall = {
-    interfaces."tornet" = {
-      allowedTCPPorts = [ 9040 9050 ];
-      allowedUDPPorts = [ 53 ];
-    };
-  };
-}
--- Duanin2Aspire/modules/software/network/sysctl/default.nix
+++
@@ -1,6 +0,0 @@
-{ ... }: {
-  boot.kernel.sysctl = {
-    "net.ipv4.conf.tornet.route_localnet" = 1;
-    "net.ipv4.conf.all.forwarding" = 1;
-  };
-}
