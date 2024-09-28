dd6863433223490ee52ef04c289450d31407dbb0
Dušan Till <tilldusan30@gmail.com>
2024-09-28 10:59:21 +0200

    HSTS increase max-age

--- RaspberryPi5/modules/software/web/nginx/default.nix
+++ RaspberryPi5/modules/software/web/nginx/default.nix
@@ -9,8 +9,7 @@
     paths = [ "/security.txt" "/.well-known/security.txt" ];
     
     finalSecurity = pkgs.writeText "security.txt" ''
-${lib.strings.concatMapStrings (path: ''Canonical: http://${origin}${path}
-Canonical: https://${origin}${path}'') paths}
+${lib.strings.concatMapStrings (path: ''Canonical: https://${origin}${path}'') paths}
 
 Contact: mailto:admin-security@duanin2.top
 
@@ -25,7 +24,7 @@ ${lib.strings.concatMapStrings (path: "location =${path} { alias ${finalSecurity
   securityHeaders = let
     allowedSrc = "'self' $scheme://duanin2.top $scheme://*.duanin2.top";
   in ''
-add_header Strict-Transport-Security "max-age=300" always;
+add_header Strict-Transport-Security "max-age=604800" always;
 add_header X-Frame-Options "DENY" always;
 add_header X-Content-Type-Options "nosniff" always;
 add_header Content-Security-Policy "default-src ${allowedSrc}; base-uri ${allowedSrc}; frame-src ${allowedSrc}; frame-ancestors ${allowedSrc}; form-action ${allowedSrc}" always;
