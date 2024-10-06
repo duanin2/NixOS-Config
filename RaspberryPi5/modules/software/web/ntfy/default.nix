89508ffdca47c6d836ffe2d493702ea20a5bcb8e
Dušan Till <tilldusan30@gmail.com>
2024-10-06 15:47:35 +0200

    Remove CSP from NTFY and allow WebSockets

--- RaspberryPi5/modules/software/web/ntfy/default.nix
+++ RaspberryPi5/modules/software/web/ntfy/default.nix
@@ -13,7 +13,7 @@
       # smtp-sender-from = "ntfy@duanin2.top";
       upstream-base-url = "https://ntfy.sh";
       visitor-request-limit-exempt-hosts = "duanin2.top,192.168.1.0/24,127.0.0.0/8";
-   };
+    };
   };
   
   services.nginx.virtualHosts = {
@@ -21,11 +21,17 @@
       useACMEHost = "duanin2.top";
       onlySSL = true;
 
-      location."/" = {
+      locations."/" = {
         proxyPass = "http://${config.services.ntfy-sh.settings.listen-http}";
+			  proxyWebsockets = true;
       };
 
-      extraConfig = (securitySetupNGINX [ "ntfy.duanin2.top" ]) + securityHeaders + httpsUpgrade + ocspStapling;
+      extraConfig = (securitySetupNGINX [ "ntfy.duanin2.top" ]) + ''
+add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
+add_header X-Frame-Options "DENY" always;
+add_header X-Content-Type-Options "nosniff" always;
+add_header Referrer-Policy "no-referrer" always;
+      '' + httpsUpgrade + ocspStapling;
     };
   };
 }
