82215bd268c351f6e42a3bc9fb07632b1266a5a1
Dušan Till <tilldusan30@gmail.com>
2024-10-27 14:30:45 +0100

    Change Dnsmasq config

--- common/modules/software/network/dnsmasq/default.nix
+++ common/modules/software/network/dnsmasq/default.nix
@@ -1,16 +1,20 @@
-{ lib, ... }: {
+{ config, lib, ... }: {
   services.dnsmasq = {
     enable = true;
 
     resolveLocalQueries = true;
     settings = {
+      listen-address = "::1,127.0.0.1,127.0.0.53";
+      cache-size = 10000;
+      conf-file = "${config.services.dnsmasq.package}/share/dnsmasq/trust-anchors.conf";
       dnssec = true;
-      use-stale-cache = 30;
+      use-stale-cache = 60;
       stop-dns-rebind = true;
       cache-rr = "ANY";
       bogus-priv = true;
-      neg-ttl = 30;
-      local-ttl = 30;
+      neg-ttl = 60;
+      local-ttl = 60;
+      log-async = 25;
       server = [
         "8.8.8.8"
         "8.8.4.4"
@@ -18,7 +22,7 @@
     };
   };
 
-  networking.resolvconf.enable = false;
+  networking.resolvconf.enable = true;
   
   services.kresd.enable = lib.mkForce false;
 }
