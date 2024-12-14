4e146bd0575672d6123521bbf34b403865c84eb0
Dušan Till <tilldusan30@gmail.com>
2024-12-14 23:37:11 +0100

    Lix HEAD

--- flake.nix
+++ flake.nix
@@ -129,9 +129,14 @@
       };
     };
     */
-		lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
+		lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 	};
 
