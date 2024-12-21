3181b55cd7e9bbccede69630722ec714ea20881a
Dušan Till <tilldusan30@gmail.com>
2024-12-21 14:09:46 +0100

    RPi5 RTC

--- RaspberryPi5/modules/software/chrony/default.nix
+++ RaspberryPi5/modules/software/chrony/default.nix
@@ -6,5 +6,8 @@
   services.chrony = {
     servers = map (value: "${toString value}.cz.pool.ntp.org") [ 0 1 2 3 ];
     serverOption = "iburst";
+    extraConfig = ''
+refclock PHC /dev/ptp0
+    '';
   };
 }
