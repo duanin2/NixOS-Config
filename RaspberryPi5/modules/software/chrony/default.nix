7d527d39441a0fc1af684580568c2a7dbf5c5657
Dušan Till <tilldusan30@gmail.com>
2024-12-22 17:18:33 +0100

    Fix Chrony RTC

--- RaspberryPi5/modules/software/chrony/default.nix
+++ RaspberryPi5/modules/software/chrony/default.nix
@@ -6,8 +6,13 @@
   services.chrony = {
     servers = map (value: "${toString value}.cz.pool.ntp.org") [ 0 1 2 3 ];
     serverOption = "iburst";
-    extraConfig = ''
-refclock PHC /dev/ptp0
-    '';
   };
+
+  boot.kernelPatches = [
+    {
+      name = "Chrony RTC Fix";
+      patch = "/dev/null";
+      extraConfig = "RTC_INTF_DEV_UIE_EMUL y";
+    }
+  ];
 }
