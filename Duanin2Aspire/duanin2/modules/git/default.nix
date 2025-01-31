22ff5c9c3dcedc5077b6d3dd4e078c83afad4d63
Dušan Till <tilldusan30@gmail.com>
2025-01-31 11:50:27 +0100

    git sendemail

--- Duanin2Aspire/duanin2/modules/git/default.nix
+++ Duanin2Aspire/duanin2/modules/git/default.nix
@@ -4,5 +4,13 @@
 
     userEmail = "duanin2-git@duanin2.top";
     userName = "Duanin2";
+
+    extraConfig = {
+      sendemail = {
+        smtpserver = "duanin2.top";
+        smtpuser = "duanin2@duanin2.top";
+        smtpencryption = "ssl";
+      };
+    };
   };
 }
